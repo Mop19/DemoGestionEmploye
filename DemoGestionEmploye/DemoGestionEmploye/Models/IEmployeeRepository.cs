2cf151a05159c9b05362dc1ad282b17f977df59a
Azure Demo <mouhameddiouf19@gmail.com>
2020-11-28 00:46:52 -0500

    Ajout List view

--- DemoGestionEmploye/DemoGestionEmploye/Models/IEmployeeRepository.cs
+++ DemoGestionEmploye/DemoGestionEmploye/Models/IEmployeeRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace DemoGestionEmploye.Models
+﻿using System.Collections.Generic;
+
+namespace DemoGestionEmploye.Models
 {
     public interface IEmployeeRepository
     {
         Employee GetEmployee(int Id);
+        IEnumerable<Employee> GetAllEmployee();
     }
 }
