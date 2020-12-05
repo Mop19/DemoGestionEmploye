e7d06f9779795a9cf73baa3d3371234620c2f00e
Azure Demo <mouhameddiouf19@gmail.com>
2020-12-04 19:47:03 -0500

    Edition user

--- DemoGestionEmploye/DemoGestionEmploye/Controllers/AdministrationController.cs
+++ DemoGestionEmploye/DemoGestionEmploye/Controllers/AdministrationController.cs
@@ -31,6 +31,63 @@ namespace DemoGestionEmploye.Controllers
         }
 
         [HttpGet]
+        public async Task<IActionResult> EditUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"L'utilisateur avec Id = {id} est introuvable";
+            }
+
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var model = new EditUserViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                UserName = user.UserName,
+                City = user.City,
+                Claims = userClaims.Select(c => c.Value).ToList(),
+                Roles = userRoles
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(EditUserViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.Id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"L'utilisateur avec Id = {model.Id} est introuvable";
+                return View("NotFound");
+            }
+            else
+            {
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+                user.City = model.City;
+
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
         public IActionResult CreateRole()
         {
             return View();
---
+++ DemoGestionEmploye/DemoGestionEmploye/ViewsModels/EditUserViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoGestionEmploye.ViewsModels
+{
+    public class EditUserViewModel
+    {
+        public EditUserViewModel()
+        {
+            Claims = new List<string>();
+            Roles = new List<string>();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nom")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name ="Ville")]
+        public string City { get; set; }
+
+        public List<string> Claims { get; set; }
+
+        public IList<string> Roles { get; set; }
+    }
+}
