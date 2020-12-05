eecbdaacc1f0dd8a007e29f7c83a624cf338b344
Azure Demo <mouhameddiouf19@gmail.com>
2020-12-04 23:06:42 -0500

    Supprime utilisateur

--- DemoGestionEmploye/DemoGestionEmploye/Controllers/AdministrationController.cs
+++ DemoGestionEmploye/DemoGestionEmploye/Controllers/AdministrationController.cs
@@ -23,6 +23,32 @@ namespace DemoGestionEmploye.Controllers
             _userManager = userManager;
         }
 
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"L'utilisateur avec Id = {id} est introuvable";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await _userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("ListUsers");
+            }
+        }
+
         [HttpGet]
         public IActionResult ListUsers()
         {
